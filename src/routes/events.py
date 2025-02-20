d219c2756139f4c1e21191f40079d027c38ac5e0
Vishvaa K <kvishvaa6@gmail.com>
2025-02-20 10:09:24 +0000

    Enhance certificate generation with registration number and event name; update HTML form for user input

--- src/routes/events.py
+++ src/routes/events.py
@@ -3,13 +3,14 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from ..extensions import mongo
 import cv2
 from datetime import datetime
+import os
 
 events = Blueprint("events",__name__,url_prefix="/events")
 
 
-def generate_certificate(name,dept,event_name):
+def generate_certificate(name,dept,event_name,regno):
     template = cv2.imread("./static/participation_certificate.png")
-
+    # Name
     text = name
     font = cv2.FONT_HERSHEY_COMPLEX
     font_scale = 1.95
@@ -20,8 +21,7 @@ def generate_certificate(name,dept,event_name):
     x = (template_width - text_width) // 2
     y = 840
     cv2.putText(template, text, (x, y), font, font_scale, color, thickness, cv2.LINE_AA)
-
-
+    # Department
     font = cv2.FONT_HERSHEY_COMPLEX
     font_scale = 1.3
     thickness = 2
@@ -30,26 +30,33 @@ def generate_certificate(name,dept,event_name):
     x = (template_width - text_width) // 2
     y = 950
     cv2.putText(template, dept, (x, y), font, font_scale, color, thickness, cv2.LINE_AA)
-
-    event = "Literary Club Event"
+    # Event
     font = cv2.FONT_HERSHEY_COMPLEX
     font_scale = 0.8
     thickness = 2
     color = (66, 154, 203)
-
-    cv2.putText(template, event, (968,1025), font, font_scale, color, thickness, cv2.LINE_AA)
-    
-
-
+    cv2.putText(template, event_name, (968,1025), font, font_scale, color, thickness, cv2.LINE_AA)
+    # Date
+    current_date = datetime.now().strftime("%d-%m-%Y")
+    font_scale = 0.8
+    thickness = 2
+    cv2.putText(template, current_date, (1304, 1023), font, font_scale, color, thickness, cv2.LINE_AA)
+    cv2.imwrite(f"./static/certificates/{regno}.png", template)
 
 
 @events.route("/",methods=["GET","POST"])
 def events_home():
     if request.method == "POST":
-        name,dept = request.form["name"],request.form["dept"]
+        name,dept,regno = request.form["name"],request.form["dept"],request.form["regno"]
         event_name = request.form.get("event")
         if not mongo.db.event_info.find_one({"event_name":event_name}):
             flash("Event does not exist")
         else:
-            pass
+            generate_certificate(name,dept,event_name,regno)
+            return redirect(url_for("events.get_certificate",regno=regno))
     return render_template("events/index.html")
+
+@events.route("/get_certificate/<regno>")
+def get_certificate(regno):
+    path = os.path.join(os.path.abspath("Quiz-App/event_certificates/"),f"{regno}.png")
+    return send_file(path,as_attachment=True)
